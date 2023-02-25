a6097efbd6557b6ff73606a4de643a2e0edb9635
Przemysław Łach <przemyslaw.lach@kitopi.com>
2023-02-25 14:32:01 +0100

    refactor: after review

--- js/helpers.js
+++ js/helpers.js
@@ -1,76 +1,76 @@
 /* functions to help with data manipulation */
-import {Forecast} from "./forecastClass";
+import { Forecast } from './forecastClass'
 
-export {normalize, extractData}
+export { normalize, extractData }
 
-const weatherTab = document.querySelector("div.module__weather")
+const weatherTab = document.querySelector('div.module__weather')
+const body = document.querySelector('body')
+
+const futureForecast = (arr) =>
+  arr.map((el) => ({
+    day: getWeekday(el.date),
+    temp: el.day.avgtemp_c,
+    condition: el.day.condition.code
+  }))
 
 function extractData(data, rawCity) {
-    let city = data['location']['name']
-    city = checkSimilarity(city, rawCity)
-    const time0fDay = dayOrNight(data['location']['localtime'])
-    const condition = data['current']['condition']['code']
-    const temp = data['current']['temp_c']
-    const pressure = data['current']['pressure_mb']
-    const humidity = data['current']['humidity']
-    const wind = convertWindSpeed(data['current']['wind_kph'])
-    const future = futureForecast(data['forecast']['forecastday'].slice(1))
+  console.log('rawCity:', rawCity)
+  console.log('data:', data)
 
-    let clonedWeatherTab = weatherTab.cloneNode(true)
-    weatherTab.parentElement.appendChild(clonedWeatherTab)
-    const forecast = new Forecast(clonedWeatherTab)
-    forecast.updateData(city, temp, pressure, humidity, wind, condition, time0fDay)
-    forecast.updateForecast(future)
-    clonedWeatherTab.hidden = false
+  localStorage.setItem('marcin', JSON.stringify({ [data.location.name]: data }))
+  let city = data['location']['name']
+  city = checkSimilarity(city, rawCity)
+  const timeOfDay = dayOrNight(data['location']['localtime'])
+  const condition = data['current']['condition']['code']
+  const temp = data['current']['temp_c']
+  const pressure = data['current']['pressure_mb']
+  const humidity = data['current']['humidity']
+  const wind = convertWindSpeed(data['current']['wind_kph'])
+  const future = futureForecast(data.forecast.forecastday.slice(1))
+
+  let clonedWeatherTab = weatherTab.cloneNode(true)
+  weatherTab.parentElement.appendChild(clonedWeatherTab)
+  const forecast = new Forecast(clonedWeatherTab)
+  forecast.updateData({ pressure, temp, city, humidity, wind, condition, timeOfDay })
+  forecast.updateForecast(future)
+  clonedWeatherTab.hidden = false
 }
 
 const checkSimilarity = (city, rawCity) => {
-    if (!rawCity) {
-        return city
-    } else if (city.toLowerCase() === normalize(rawCity).toLowerCase()) {
-        return capitalize(rawCity)
-    } else {
-        return city
-    }
+  if (!rawCity) {
+    return city
+  } else if (city.toLowerCase() === normalize(rawCity).toLowerCase()) {
+    return capitalize(rawCity)
+  }
+
+  return city
 }
 
-const normalize = (input) => {
-    let result = input.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-    result = result.replace(/\u0142/g, "l")
-    return result
+const normalize = (value) => {
+  let result = value.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+  return result.replace(/\u0142/g, 'l')
 }
 
 const capitalize = (s) => {
-    if (typeof s !== 'string') return ''
-    let result = s.split(" ").map(word => {
-        return word[0].toUpperCase() + word.slice(1)
-    })
-    return result.join(" ")
+  if (typeof s !== 'string') return ''
+  let result = s.split(' ').map((word) => {
+    return word[0].toUpperCase() + word.slice(1)
+  })
+  return result.join(' ')
 }
 
 const dayOrNight = (date) => {
-    let hour = new Date(date).getHours()
-    if (6 < hour && hour < 20) {
-        return "day"
-    } else {
-        return "night"
-    }
+  let hour = new Date(date).getHours()
+  if (6 < hour && hour < 20) {
+    return 'day'
+  } else {
+    return 'night'
+  }
 }
 
-const convertWindSpeed = (kph) => {
-    return Math.round(kph / 3.6 * 10) / 10
-}
+const convertWindSpeed = (kph) => Math.round((kph / 3.6) * 10) / 10
 
-const futureForecast = (arr) => {
-    return arr.map(day => {
-        return {
-            day: getWeekday(day['date']),
-            temp: day['day']['avgtemp_c'],
-            condition: day['day']['condition']['code']
-        }
-    })
-}
+const getWeekday = (date) => capitalize(new Date(date).toLocaleString('PL', { weekday: 'long' }))
 
-const getWeekday = (date) => {
-    return capitalize(new Date(date).toLocaleString('PL', {weekday: 'long'}))
-}
+export const load = () => body.classList.add('loading')
+export const unload = () => body.classList.remove('loading')
